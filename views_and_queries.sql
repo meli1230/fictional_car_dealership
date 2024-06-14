358070efbc1fc7a7018a40efd679b454b2c310a7
Melisa <150281139+meli1230@users.noreply.github.com>
2024-06-14 21:43:58 +0300

    Update views_and_queries.sql

--- views_and_queries.sql
+++ views_and_queries.sql
@@ -1,7 +1,6 @@
 -- VIEWS
 
--- 1. View cu 2 tabele care afiseaza seria de sasiu, modelul auto si pretul autovehiculelor impreuna cu reprezentantele care le-au vandut
-
+-- 1. View with 2 tables that display the chassis number, the vehicle model and the vehicle price, alongside with the dealerships that sold them
 CREATE VIEW cars_sold_by_dealer AS
 SELECT 
     vehicles.chassis_nb, 
@@ -12,12 +11,13 @@ FROM vehicles
 LEFT JOIN dealers ON vehicles.id_rpa = dealers.id;
 SELECT * FROM cars_sold_by_dealer;
 
--- Interogare care afiseaza instantele din view-ul de mai sus cu un pret de vanzare mai mare decat un number introdus de la tastatura
-SELECT * FROM cars_sold_by_dealer WHERE price > :pret_introdus;
+-- Query that displays the delerships from the created view, that have a total sum greater than a an input number
+Interogare care afiseaza instantele din view-ul de mai sus cu un pret de vanzare mai mare decat un number introdus de la tastatura
+SELECT * FROM cars_sold_by_dealer WHERE price > :total_sum;
 
 
 
--- 2. View cu 2 tabele care afseaza clientii si departamentele din care provin
+-- 2. View with 2 tables that display the clients and their departments
 CREATE VIEW dealers_clients AS
 SELECT 
     clients.name,
@@ -28,13 +28,14 @@ FROM clients
 JOIN cities ON clients.id_ors = cities.id;
 SELECT * FROM dealers_clients;
 
--- Interogare care afiseaza view-ul de mai sus in ordine alfabetica a clientilor
+-- Query that displayes the data in the created view in alphabetical order of the clients' names
 SELECT * FROM dealers_clients ORDER BY name ASC, surname ASC;
 
 
 
--- 3. View cu 3 tabele care afiseaza atat autovehiculele vandute, cat si cele nevandute. 
--- In cazul autovehiculelor vandute, se va afisa numberul facturii, data si ora facturii, name si surname client, iar pentru toate autovehiculele se va afisa model autovehicul si pretul de vanzare.
+-- 3. View with 3 tables that displays the sold vehicles, as well as the ones that were not sold yet.
+-- In the case of the sold vehicles, we will also display the invoic number, the date and time of the purchase, the name of the client.
+-- We will also display the vehicle model and the selling price.
 CREATE VIEW sale_details AS 
 SELECT 
     sales.invoice_nb, 
@@ -48,12 +49,12 @@ FULL JOIN clients ON sales.id_cet = clients.id
 FULL JOIN vehicles ON sales.chassis_nb_aul = vehicles.chassis_nb;
 SELECT * FROM sale_details;
 
--- Interogare care afiseaza doar autovehiculele vandute din view-ul de mai sus
+-- Query that displays the sold vehicles only, using the view above.
 SELECT * FROM sale_details WHERE invoice_nb IS NOT NULL;
 
 
 
--- 4. View cu 3 tabele care afiseaza reprezentantele si valoarea autovehiculelor vandute de ei pana in prezent
+-- 4. View with 3 tables that displays the dealerships and the value of vehicles that each one of them sold
 CREATE VIEW dealer_total_sales AS
 SELECT 
     dealers.id,
@@ -66,5 +67,5 @@ JOIN sales ON vehicles.chassis_nb = sales.chassis_nb_aul
 GROUP BY dealers.id, dealers.name, dealers.address; 
 SELECT * FROM dealer_total_sales;
 
--- Interogare care afiseaza reprezentantele din view-ul de mai sus in ordinea valorii vanzarilor efectuate
+-- Query that displays the dealerships from the created viewm, sorting them by the value of their sells, starting with the biggest value
 SELECT * FROM dealer_total_sales ORDER BY valoare_vanzari DESC;
